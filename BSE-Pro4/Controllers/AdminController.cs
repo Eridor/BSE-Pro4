5d1003cefd50a6a2aac3846133ead842e9be857e
Michał <eridor@home.pl>
2017-05-05 16:58:19 +0200

    Zarządzanie userami i rolami

--- BSE-Pro4/Controllers/AdminController.cs
+++ BSE-Pro4/Controllers/AdminController.cs
@@ -1,17 +1,235 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using BSE_Pro4.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace BSE_Pro4.Controllers
 {
     public class AdminController : Controller
     {
+        ApplicationDbContext _db = new ApplicationDbContext();
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+        public AdminController()
+        {
+        }
+        public AdminController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+
         // GET: Admin
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult UsersList()
+        {
+            return View(_db.Users.ToList());
+        }
+        public ActionResult UserEdit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser applicationUser = _db.Users.Find(id);
+            if (applicationUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(applicationUser);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UserEdit(
+            [Bind(
+                Include =
+                    "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName"
+                )] ApplicationUser applicationUser)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(applicationUser).State = EntityState.Modified;
+                _db.SaveChanges();
+            }
+            return RedirectToAction("UserEdit");
+        }
+
+        public ActionResult UserDelete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ApplicationUser usr = _db.Users.Find(id);
+            if (usr == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usr);
+        }
+
+        [HttpPost, ActionName("UserDelete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult UDelete(string id)
+        {
+            ApplicationUser usr = _db.Users.Find(id);
+            _db.Users.Remove(usr);
+            _db.SaveChanges();
+            return RedirectToAction("UsersList");
+        }
+
+        public ActionResult UserCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UserCreate(
+            [Bind(
+                Include =
+                    "Id,Email,EmailConfirmed,PasswordHash,SecurityStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEndDateUtc,LockoutEnabled,AccessFailedCount,UserName"
+                )] ApplicationUser usr)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Users.Add(usr);
+                //_db.SaveChanges();
+                return RedirectToAction("UsersList");
+            }
+            return View(usr);
+        }
+
+        ///
+        public ActionResult RoleList()
+        {
+            return View(_db.Roles.ToList());
+        }
+
+        public ActionResult RoleCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RoleCreate(FormCollection collection)
+        {
+
+            _db.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole()
+            {
+                Name = collection["RoleName"]
+            });
+            _db.SaveChanges();
+            ViewBag.ResultMessage = "Role created successfully !";
+            return RedirectToAction("RoleList");
+        }
+
+        public ActionResult RoleDelete(string RoleName)
+        {
+            IdentityRole Role = _db.Roles.FirstOrDefault(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase));
+            _db.Roles.Remove(Role);
+            _db.SaveChanges();
+            return RedirectToAction("RoleList");
+        }
+
+        public ActionResult ManageUserRoles()
+        {
+            // prepopulat roles for the view dropdown
+            var list = _db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RoleAddToUser(string UserName, string RoleName)
+        {
+            ApplicationUser user = _db.Users.FirstOrDefault(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase));
+            var account = new AccountController(UserManager, SignInManager);
+            account.UserManager.AddToRole(user.Id, RoleName);
+
+            ViewBag.ResultMessage = "Role created successfully !";
+
+            // prepopulat roles for the view dropdown
+            var list = _db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+
+            return View("ManageUserRoles");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetRoles(string UserName)
+        {
+            if (!string.IsNullOrWhiteSpace(UserName))
+            {
+                ApplicationUser user = _db.Users.FirstOrDefault(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase));
+                var account = new AccountController(UserManager, SignInManager);
+
+                ViewBag.RolesForThisUser = account.UserManager.GetRoles(user.Id);
+
+                // prepopulat roles for the view dropdown
+                var list = _db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+                ViewBag.Roles = list;
+            }
+
+            return View("ManageUserRoles");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteRoleForUser(string UserName, string RoleName)
+        {
+            var account = new AccountController(UserManager, SignInManager);
+            ApplicationUser user = _db.Users.FirstOrDefault(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase));
+
+            if (account.UserManager.IsInRole(user.Id, RoleName))
+            {
+                account.UserManager.RemoveFromRole(user.Id, RoleName);
+                ViewBag.ResultMessage = "Role removed from this user successfully !";
+            }
+            else
+            {
+                ViewBag.ResultMessage = "This user doesn't belong to selected role.";
+            }
+            // prepopulat roles for the view dropdown
+            var list = _db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+            ViewBag.Roles = list;
+
+            return View("ManageUserRoles");
         }
     }
 }
