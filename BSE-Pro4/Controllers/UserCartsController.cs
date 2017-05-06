03be34ac9fe19d16ac3ae6e80e79c5df252fd434
tangoandcash <kajak937@wp.pl>
2017-05-06 20:25:33 +0200

    Polonizacja i początek koszyka klienta

---
+++ BSE-Pro4/Controllers/UserCartsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BSE_Pro4.Models;
+
+namespace BSE_Pro4.Controllers
+{
+    public class UserCartsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: UserCarts
+        public ActionResult Index()
+        {
+            var carts = db.Carts.Include(c => c.ProductItem).Include(c => c.User);
+            return View(carts.ToList());
+        }
+
+        // GET: UserCarts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cart cart = db.Carts.Find(id);
+            if (cart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cart);
+        }
+
+        // GET: UserCarts/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Name");
+            ViewBag.UserID = new SelectList(db.Users, "Id", "Email");
+            return View();
+        }
+
+        // POST: UserCarts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CartId,UserID,ProductId,Quantity")] Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Carts.Add(cart);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Name", cart.ProductId);
+            ViewBag.UserID = new SelectList(db.Users, "Id", "Email", cart.UserID);
+            return View(cart);
+        }
+
+        // GET: UserCarts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cart cart = db.Carts.Find(id);
+            if (cart == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Name", cart.ProductId);
+            ViewBag.UserID = new SelectList(db.Users, "Id", "Email", cart.UserID);
+            return View(cart);
+        }
+
+        // POST: UserCarts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CartId,UserID,ProductId,Quantity")] Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cart).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProductId = new SelectList(db.Products, "ProductId", "Name", cart.ProductId);
+            ViewBag.UserID = new SelectList(db.Users, "Id", "Email", cart.UserID);
+            return View(cart);
+        }
+
+        // GET: UserCarts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cart cart = db.Carts.Find(id);
+            if (cart == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cart);
+        }
+
+        // POST: UserCarts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cart cart = db.Carts.Find(id);
+            db.Carts.Remove(cart);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
