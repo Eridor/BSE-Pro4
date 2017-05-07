9c7c881d6d28e1941b07f89a2b68ff9129e242b9
Michał <eridor@home.pl>
2017-05-07 18:08:44 +0200

    Baza transakcji

---
+++ BSE-Pro4/Controllers/UserTransactionsController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BSE_Pro4.Models;
+
+namespace BSE_Pro4.Controllers
+{
+    public class UserTransactionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: UserTransactions
+        public ActionResult Index()
+        {
+            var transactions = db.Transactions.Include(t => t.TransactionStatus).Include(t => t.User).Include(t => t.UserInvoice).Include(t => t.UserShipment);
+            return View(transactions.ToList());
+        }
+
+        // GET: UserTransactions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            return View(transaction);
+        }
+
+        // GET: UserTransactions/Create
+        public ActionResult Create()
+        {
+            ViewBag.TransactionStatusId = new SelectList(db.TransactionStatus, "TransactionStatusId", "Description");
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email");
+            ViewBag.UserInvoiceId = new SelectList(db.UserShipments, "UserShipId", "UserID");
+            ViewBag.UserShipmentId = new SelectList(db.UserShipments, "UserShipId", "UserID");
+            return View();
+        }
+
+        // POST: UserTransactions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TransactionId,UserId,UserShipmentId,UserInvoiceId,TotalCost,TransactionStatusId")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Transactions.Add(transaction);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TransactionStatusId = new SelectList(db.TransactionStatus, "TransactionStatusId", "Description", transaction.TransactionStatusId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", transaction.UserId);
+            ViewBag.UserInvoiceId = new SelectList(db.UserShipments, "UserShipId", "UserID", transaction.UserInvoiceId);
+            ViewBag.UserShipmentId = new SelectList(db.UserShipments, "UserShipId", "UserID", transaction.UserShipmentId);
+            return View(transaction);
+        }
+
+        // GET: UserTransactions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Transaction transaction = db.Transactions.Find(id);
+            if (transaction == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TransactionStatusId = new SelectList(db.TransactionStatus, "TransactionStatusId", "Description", transaction.TransactionStatusId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", transaction.UserId);
+            ViewBag.UserInvoiceId = new SelectList(db.UserShipments, "UserShipId", "UserID", transaction.UserInvoiceId);
+            ViewBag.UserShipmentId = new SelectList(db.UserShipments, "UserShipId", "UserID", transaction.UserShipmentId);
+            return View(transaction);
+        }
+
+        // POST: UserTransactions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TransactionId,UserId,UserShipmentId,UserInvoiceId,TotalCost,TransactionStatusId")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(transaction).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TransactionStatusId = new SelectList(db.TransactionStatus, "TransactionStatusId", "Description", transaction.TransactionStatusId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "Email", transaction.UserId);
+            ViewBag.UserInvoiceId = new SelectList(db.UserShipments, "UserShipId", "UserID", transaction.UserInvoiceId);
+            ViewBag.UserShipmentId = new SelectList(db.UserShipments, "UserShipId", "UserID", transaction.UserShipmentId);
+            return View(transaction);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
